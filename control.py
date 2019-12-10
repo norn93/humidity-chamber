b048b802960768595aee0c7c83fbbe8f054d1b22
George Knowlden <george.knowlden@gmail.com>
2019-12-10 17:36:33 +0800

    Testing pathing

--- control.py
+++ control.py
@@ -1,5 +1,11 @@
 import switch
 import data
+import datetime
+
+HOME_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + "/"
+LOG_FILE = HOME_DIR + "log.log"
+
+print(LOG_FILE)
 
 while True:
 	t, h = data.get_temperature_humidity()
