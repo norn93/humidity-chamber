6685aa751ca51842d5dfb60ec3d032188ff10468
George Knowlden <george.knowlden@gmail.com>
2019-12-10 17:55:51 +0800

    Logging enabled

--- control.py
+++ control.py
@@ -1,12 +1,22 @@
 import switch
 import data
-import datetime
+import time, datetime
+import os, sys
+
+DELAY_TIME = 10
 
 HOME_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + "/"
 LOG_FILE = HOME_DIR + "log.log"
 
-print(LOG_FILE)
+def loop():
+    today = str(datetime.datetime.now())
+    t, h = data.get_temperature_humidity()
+    temperature = str(round(t, 2))
+    humidity = str(round(h, 2))
+    print(temperature, humidity)
+    with open(LOG_FILE, "a+") as f:
+        f.write(today + ", " + temperature + ", " + humidity + "\n")
 
 while True:
-	t, h = data.get_temperature_humidity()
-	print(t, h)
+    loop()
+    time.sleep(DELAY_TIME)
