424f12fdf0b3684d76db22eb0fe34f17de974193
George Knowlden <george.knowlden@gmail.com>
2019-12-10 17:15:09 +0800

    New program structure

---
+++ control.py
@@ -0,0 +1,6 @@
+import switch
+import data
+
+while True:
+	t, h = data.get_temperature_humidity()
+	print(t, h)
--- data.py
+++ data.py
@@ -3,10 +3,10 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-while True:
+def get_temperature_humidity():
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        return temperature, humidity
     else:
-        print("Failed to retrieve data from humidity sensor")
+        pass
